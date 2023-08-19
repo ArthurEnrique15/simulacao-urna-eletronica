3c52c3540c06c17da3840c28248ccc3ac6f29d7e
Arthur Enrique Ribeiro Silva <arthur.silva@gcbinvestimentos.com>
2023-08-19 20:40:01 -0300

    test: correct infra tests

--- backend/src/infra/cryptography/bcrypt-adapter/index.spec.ts
+++ backend/src/infra/cryptography/bcrypt-adapter/index.spec.ts
@@ -10,6 +10,7 @@ describe('BcryptAdapter', () => {
   beforeAll(() => {
     sut = new BcryptAdapter(salt)
     jest.spyOn(bcrypt, 'hash').mockImplementation(() => 'hashed_value')
+    jest.spyOn(bcrypt, 'compare').mockImplementation(() => true)
   })
 
   describe('hash', () => {
@@ -18,9 +19,27 @@ describe('BcryptAdapter', () => {
       expect(bcrypt.hash).toHaveBeenCalledWith('any_value', salt)
     })
 
-    it('should return a token on sign success', async () => {
+    it('should return hash response', async () => {
       const response = sut.hash('any_value')
       expect(response).toEqual('hashed_value')
+    })
+  })
+
+  describe('compare', () => {
+    it('should call compare with correct values', async () => {
+      sut.compare({ value: 'any_value', hash: 'any_hash' })
+      expect(bcrypt.compare).toHaveBeenCalledWith('any_value', 'any_hash')
+    })
+
+    it('should return true if compare returns true', async () => {
+      const response = sut.compare({ value: 'any_value', hash: 'any_hash' })
+      expect(response).toEqual(true)
+    })
+
+    it('should return false if compare returns false', async () => {
+      jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => false)
+      const response = sut.compare({ value: 'any_value', hash: 'any_hash' })
+      expect(response).toEqual(false)
     })
   })
 })
--- backend/src/infra/cryptography/jwt-adapter/index.spec.ts
+++ backend/src/infra/cryptography/jwt-adapter/index.spec.ts
@@ -5,9 +5,13 @@ import { JwtAdapter } from '.'
 describe('JwtAdapter', () => {
   let sut: JwtAdapter
 
+  const validToken = 'any_token'
+  const validDecryptedToken = { id: 'any_id' }
+
   beforeAll(() => {
     sut = new JwtAdapter('secret')
-    jest.spyOn(jwt, 'sign').mockImplementation(() => 'any_token')
+    jest.spyOn(jwt, 'sign').mockImplementation(() => validToken)
+    jest.spyOn(jwt, 'verify').mockImplementation(() => validDecryptedToken)
   })
 
   describe('encrypt', () => {
@@ -18,7 +22,19 @@ describe('JwtAdapter', () => {
 
     it('should return a token on sign success', async () => {
       const response = await sut.encrypt({ id: 'any_id' })
-      expect(response).toEqual('any_token')
+      expect(response).toEqual(validToken)
+    })
+  })
+
+  describe('decrypt', () => {
+    it('should call verify with correct values', async () => {
+      await sut.decrypt(validToken)
+      expect(jwt.verify).toHaveBeenCalledWith(validToken, 'secret')
+    })
+
+    it('should return the decrypted token', async () => {
+      const response = await sut.decrypt(validToken)
+      expect(response).toEqual(validDecryptedToken)
     })
   })
 })
