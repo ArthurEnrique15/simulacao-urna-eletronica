4a9a6ed8cf1231ff0dc7a4bd6b63339f5fab0f5d
Arthur Enrique Ribeiro Silva <arthur.silva@gcbinvestimentos.com>
2023-08-19 20:20:39 -0300

    fix: name folder correctly

--- backend/src/main/factories/use-cases/list-candidates.ts
+++ backend/src/main/factories/use-cases/list-candidates.ts
@@ -1,4 +1,4 @@
-import { ListCandidates } from '@/data/use-cases/list-dandidates'
+import { ListCandidates } from '@/data/use-cases/list-candidates'
 import { IListCandidates } from '@/domain/use-cases/list-candidates'
 
 import { makeCandidateRepository } from '../database/mongodb/candidate-repository'
