f7f443dddef1df20957792ead60588ad320f8afa
Arthur Enrique Ribeiro Silva <arthur.silva@gcbinvestimentos.com>
2023-08-20 12:41:51 -0300

    feat: add route protection based on logged user

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,13 +1,30 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import { Urn } from './pages/Urn'
+import { Login } from './pages/Login'
+import { Register } from './pages/Register'
+import { useContext } from 'react'
+import { LoggedUserContext } from './contexts/LoggedUserContext'
 
 export function Router() {
+  const { loggedUser } = useContext(LoggedUserContext)
+
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={<Urn />} />
+        <Route
+          path="/"
+          element={!loggedUser ? <Navigate to="/login" /> : <Urn />}
+        />
       </Route>
+      <Route
+        path="/login"
+        element={loggedUser ? <Navigate to="/" /> : <Login />}
+      />
+      <Route
+        path="/register"
+        element={loggedUser ? <Navigate to="/" /> : <Register />}
+      />
     </Routes>
   )
 }
